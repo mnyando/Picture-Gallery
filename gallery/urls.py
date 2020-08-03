f6f785e484ac469e17c9ff06632ec0475f30327d
Mathews Alufwani <mathewalufwani@gmail.com>
2020-08-03 15:29:16 +0300

    add location url

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^photos/(\d+)', views.photos, name='photos'),
+    url(r'^location/(\d+)',views.location,name = 'location'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
