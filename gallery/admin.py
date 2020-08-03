e403c16e9ffb9b24220f94264474eed8c21c2d19
Mathews Alufwani <mathewalufwani@gmail.com>
2020-08-03 07:33:22 +0300

    add search bar

--- gallery/admin.py
+++ gallery/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import Tag, Photos, Location, Category
 
 # Register your models here.
-admin.site.register(Photos)
+
+class PhotoAdmin(admin.ModelAdmin):
+    filter_horizontal =('tag',)
+
+admin.site.register(Photos, PhotoAdmin)
 admin.site.register(Category)
 admin.site.register(Location)
 admin.site.register(Tag)
