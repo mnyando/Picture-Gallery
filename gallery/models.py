e405725b112c14ea1354f94050519332ee5a6bd5
Mathews Alufwani <mathewalufwani@gmail.com>
2020-08-03 05:51:06 +0300

    add models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Photos(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    tag = models.ManyToManyField(tag)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    photo_image = models.ImageField(upload_to = 'photos/')
