67817714c010754abfb803a12284e55ca10173ed
Mathews Alufwani <mathewalufwani@gmail.com>
2020-08-03 07:01:06 +0300

    create superuser

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tag, Photos, Location, Category
 
 # Register your models here.
+admin.site.register(Photos)
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Tag)
+
