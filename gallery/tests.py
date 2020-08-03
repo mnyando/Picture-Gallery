9ee7166ca73ab04dbfb20f3affb7e0b963b7a6c2
Mathews Alufwani <mathewalufwani@gmail.com>
2020-08-03 06:09:53 +0300

    add location tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -9,3 +9,25 @@ class TagTestCase(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.life, Tag))
+
+class LocationTest(TestCase):
+    def setUp(self):
+        '''new instance before test'''
+        self.nairobi = Location(name='nairobi')
+
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_location_instance(self):
+        self.assertTrue(isinstance(self.nairobi, Location))
+
+    def test_save_location(self):
+        self.nairobi.save_location()
+        location = Location.objects.all()
+        self.assertEqual(len(location), 1)
+
+    def test_update_location(self):
+        self.nairobi.save_location()
+        self.nairobi.update_location(self.nairobi.id,'maldives')
+        update = Location.objects.get(name = "maldives")
+        self.assertEqual(update.name, 'maldives')
