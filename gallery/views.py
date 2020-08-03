ec267e7203c6c72dbc297b2d70dc238b59d2f333
Mathews Alufwani <mathewalufwani@gmail.com>
2020-08-03 13:27:18 +0300

    add background

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
+from .models import Photos, Location
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    photos = Photos.todays_album()
+    return render(request, 'index.html', {"photos": photos})
+
+def search_results(request):
+
+    if 'photos' in request.GET and request.GET["photos"]:
+        search_term = request.GET.get("photos")
+        searched_photos = Photos.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"photos": searched_photos})
+
+    else:
+        return render(request, 'search.html')
